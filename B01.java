ed41b1ab7062f440a1cac9e44406382f6795c82c
Soap <lee141567@naver.com>
2016-09-23 12:29:09 +0900

    B01.java 과제#2 1번 문제 완성

--- B01.java
+++ B01.java
@@ -1,6 +1,23 @@
+import java.util.*;
+
 class B01 {
-	public static void main(String[] args){
-		System.out.println("갸아악");
+	int age;
+	void print(){
+		if(this.age > 20)
+			System.out.println("Adult");
+		else
+			System.out.println("Not Adult");
 	}
+	void input(){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input your birth : ");
+		int year = s.nextInt();
+		this.age = 2016 - year + 1;
+		print();
+	}
+	public static void main(String[] args){ 
+		new B01().input();
+	}
+
 }
 
