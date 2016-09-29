1c58d65f0c30412ae7c66ed6777771a63e428c76
soapmaze <lee141567@naver.com>
2016-09-30 02:55:28 +0900

    B03.java 완성

---
+++ B03.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class B03{
+	int days;
+	int seconds;
+	int m_count;
+
+	void output(){
+		this.seconds = this.days * 24 * 60 * 60;
+		this.m_count = seconds / 1000000;
+		System.out.printf("days to seconds : %d\n", seconds);
+		System.out.printf("seconds having million seconds time : %d\n", m_count);
+	}
+
+	void input(){
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input Days : ");
+		this.days = s.nextInt();
+		
+		output();
+
+	}
+
+	public static void main(String[] args){
+		new B03().input();		
+	}
+}
